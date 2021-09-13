6171c2287e4807b87a79f177d9f297d7c30beb2d
bieqiutian <>
2021-09-13 18:36:52 +0800

    优化代码结构，重写方法功能

--- QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper.h
+++ QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper.h
@@ -10,34 +10,102 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// NSDate 相关工具
 @interface QTDateCalculationHelper : NSObject
 
-// format @"yyyyMMdd HHmmss"
 
-// 获取现在日期 0时0分0秒,因为计算周期按照自然日
-+ (NSString *)getNowdate;
+#pragma mark 字符串与NSDate之间转换 ↓↓↓
 
-// 获取现在时间
-+ (NSString *)getNowTime;
+/// =============== 将字符串转为NSDate对象 ===============
+/// @param timeString 时间字符串
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日
++(NSDate *)getDateWithTimeString:(NSString *)timeString
+                          format:(NSString *)formatString;
 
-// 计算与今天相隔几天,正数今天之后,负数今天之前
-+ (NSInteger)daysWithTime:(NSString *)time;
+/// =============== 将NSDate对象转换为字符串 ===============
+/// @param date 时间对象
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日
++(NSString *)getTimeStringWithDate:(NSDate *)date
+                          format:(NSString *)formatString;
 
-// 计算两个日期相差几天
-+ (NSInteger)getDaysAfterTime:(NSString *)firstTime  beforeTime:(NSString *)lastTime;
 
-//获取当前日期星期几
-+ (NSInteger)getWeekNumberWithTime:(NSString *)time;
+#pragma mark 常用方法 ↓↓↓
+/// =============== 获取当前时间字符串 ===============
+/// @param formatString 时间格式,例如:yyyy年MM月dd日
++ (NSString *)getNowDateStringWithFormat:(NSString *)formatString;
 
-//日期计算
-+ (NSString *)getTimeAfter1WeekWithBeginTime:(NSString *)beginTime; //返回1周后的日期
-+ (NSString *)getTimeAfter1MonthWithBeginTime:(NSString *)beginTime; //返回下个月的一号
-+ (NSString *)getTimeWithBeginTime:(NSString *)beginTime withMonths:(NSInteger) months; //返回n月后的一号
-+ (NSString *)getTimeWithBeginTime:(NSString *)beginTime withYears:(NSInteger) Years; //返回n年后的日期
-+ (NSString *)getTimeWithBeginTime:(NSString *)beginTime withDays:(NSInteger) days;    // 返回n天后的日期
 
-//将时间文字转换成多久之前文字
-- (NSString *)formatDateBeforeWithStr:(NSString *)str;
+
+/// =============== 获取目标日期星期几 ===============
+/// @param timeString 目标时间,字符串类型
+/// @param formatString 解析时间字符串的格式,例如:yyyy年MM月dd日
++ (NSInteger)getWeekNumberWithTime:(NSString *)timeString
+                            format:(NSString *)formatString;
+
+/// 获取月份天数
+/// @param timeString 目标时间字符串
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月
++ (NSInteger)getMonthMaxDayWithTimeString:(NSString *)timeString
+                                   format:(NSString *)formatString;
+
+#pragma mark - 日期计算 ↓↓↓
+
+
+/// =============== 计算与今天相隔几天,正数今天之后,负数今天之前 ===============
+/// @param timeString 目标时间,字符串类型
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日
++ (NSInteger)daysWithTime:(NSString *)timeString
+                  format:(NSString *)formatString;
+
+
+/// =============== 计算两个时间相差几天 ===============
+/// @param firstTimeString 第一个时间字符串
+/// @param lastTimeString 第二个时间字符串
+/// @param formatString 解析时间字符串的格式,例如:yyyy年MM月dd日
++ (NSInteger)getDaysAfterTime:(NSString *)firstTimeString
+                  beforeTime:(NSString *)lastTimeString
+                      format:(NSString *)formatString;
+
+/// =============== 计算n周后的时间 ===============
+/// @param timeString 开始计算的日期
+/// @param weekCount 要计算几个星期
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)getTimeAfterWeekCountWithTimeString:(NSString *)timeString
+                                        weekCount:(NSInteger)weekCount
+                                           format:(NSString *)formatString;
+
+/// =============== 返回n年,n月,n日后的时间 ===============
+/// @param timeString 开始计算的日期
+/// @param year 年数
+/// @param month 月数
+/// @param day 天数
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)getTimeWithTimeString:(NSString *)timeString
+                               year:(NSInteger)year
+                              month:(NSInteger)month
+                                day:(NSInteger)day
+                             format:(NSString *)formatString;
+
+
+/// =============== 返回n时,n分,n秒后的时间 ===============
+/// @param timeString 开始计算的日期
+/// @param hour 小时数
+/// @param minute 分钟数
+/// @param second 秒数
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)getTimeWithTimeString:(NSString *)timeString
+                               hour:(NSInteger)hour
+                             minute:(NSInteger)minute
+                             second:(NSInteger)second
+                             format:(NSString *)formatString;
+
+
+
+/// =============== 将时间文字转换成多久之前文字 ===============
+/// @param timeString 开始计算的日期
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)formatDateBeforeWithTimeString:(NSString *)timeString
+                                      format:(NSString *)formatString;
 
 @end
 
--- QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper.m
+++ QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper/QTDateCalculationHelper.m
@@ -9,44 +9,118 @@
 #import "QTDateCalculationHelper.h"
 
 @implementation QTDateCalculationHelper
-+ (NSString *)getNowdate
+
+
+#pragma mark 字符串与NSDate之间转换 ↓↓↓
+
+/// 将字符串转为NSDate对象
+/// @param timeString 时间字符串
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日
++(NSDate *)getDateWithTimeString:(NSString *)timeString
+                          format:(NSString *)formatString
 {
     NSDateFormatter* formater = [[NSDateFormatter alloc] init];
-    [formater setDateFormat:@"yyyyMMdd 000000"];//根据自己的需求定义格式
-    formater .timeZone = [NSTimeZone systemTimeZone];
+    [formater setDateFormat:formatString];//根据自己的需求定义格式
+    formater.timeZone = [NSTimeZone systemTimeZone];
+    NSDate * date = [formater dateFromString:timeString];
+    return date;
+}
+
+/// 将NSDate对象转换为字符串
+/// @param date 时间对象
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日
++(NSString *)getTimeStringWithDate:(NSDate *)date
+                            format:(NSString *)formatString
+{
+    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
+    [formater setDateFormat:formatString];//根据自己的需求定义格式
+    formater.timeZone = [NSTimeZone systemTimeZone];
+    return [formater stringFromDate:date];
+}
+
+#pragma mark 常用方法 ↓↓↓
+
+/// =============== 获取当前时间字符串 ===============
+/// @param formatString 时间格式,例如:yyyy年MM月dd日
++ (NSString *)getNowDateStringWithFormat:(NSString *)formatString
+{
+    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
+    [formater setDateFormat:formatString];//根据自己的需求定义格式
+    formater.timeZone = [NSTimeZone systemTimeZone];
     NSDate* now = [NSDate date];
     
     return [formater stringFromDate:now];
 }
 
-
-+ (NSString *)getNowTime
+/// 获取目标日期星期几
+/// @param timeString 目标时间,字符串类型
+/// @param formatString 解析时间字符串的格式,例如:yyyy年MM月dd日
++ (NSInteger)getWeekNumberWithTime:(NSString *)timeString
+                            format:(NSString *)formatString
 {
-    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
-    [formater setDateFormat:@"yyyyMMdd HHmmss"];//根据自己的需求定义格式
-    formater .timeZone = [NSTimeZone systemTimeZone];
-    NSDate* now = [NSDate date];
     
-    return [formater stringFromDate:now];
+    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
+    formater .timeZone = [NSTimeZone systemTimeZone];
+    [formater setDateFormat:formatString];//根据自己的需求定义格式
+    
+    NSDate *inputDate = [formater dateFromString:timeString];
+    
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+    [calendar setTimeZone: timeZone];
+    
+    NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;
+    
+    NSDateComponents *theComponents = [calendar components:calendarUnit fromDate:inputDate];
+    
+    return theComponents.weekday;
 }
 
-// 计算两个日期相差几天
-+ (NSInteger)getDaysAfterTime:(NSString *)firstTime  beforeTime:(NSString *)lastTime
+
+/// 获取月份天数
+/// @param timeString 目标时间字符串
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日
++ (NSInteger)getMonthMaxDayWithTimeString:(NSString *)timeString
+                                   format:(NSString *)formatString
 {
-    //    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
-    //    formater .timeZone = [NSTimeZone systemTimeZone];
-    //    [formater setDateFormat:@"yyyyMMdd HHmmss"];//根据自己的需求定义格式
-    //    NSDate* startDate = [formater dateFromString:firstTime];
-    //    NSDate* endDate = [formater dateFromString:lastTime];
-    //    NSTimeInterval time = [endDate timeIntervalSinceDate:startDate];
-    //    CGFloat f = time / 86400.0;
-    //
-    //    return [f inter];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSRange range = [calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:[QTDateCalculationHelper getDateWithTimeString:timeString format:formatString]];
+    
+    return range.length;
+}
+
+#pragma mark - 日期计算 ↓↓↓
+
+/// 计算与今天相隔几天,正数今天之后,负数今天之前
+/// @param timeString 目标时间的字符串
+/// @param formatString 解析目标时间的格式,例如:yyyy年MM月dd日 HH:mm:sss
++ (NSInteger)daysWithTime:(NSString *)timeString
+                   format:(NSString *)formatString
+{
+    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
+    [formater setDateFormat:formatString];//根据自己的需求定义格式
+    NSDate* startDate = [NSDate date];
+    NSDate* endDate = [formater dateFromString:timeString];
+    NSTimeInterval times = [endDate timeIntervalSinceDate:startDate];
+    return times / 86400;
+}
+
+
+/// 计算两个时间相差几天
+/// @param firstTimeString 第一个时间字符串
+/// @param lastTimeString 第二个时间字符串
+/// @param formatString 解析时间字符串的格式,例如:yyyy年MM月dd日
++ (NSInteger)getDaysAfterTime:(NSString *)firstTimeString
+                   beforeTime:(NSString *)lastTimeString
+                       format:(NSString *)formatString
+{
     
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"yyyyMMdd HHmmss"];
-    NSDate *startDate = [dateFormatter dateFromString:firstTime];
-    NSDate *endDate = [dateFormatter dateFromString:lastTime];
+    [dateFormatter setDateFormat:formatString];
+    NSDate *startDate = [dateFormatter dateFromString:firstTimeString];
+    NSDate *endDate = [dateFormatter dateFromString:lastTimeString];
     //利用NSCalendar比较日期的差异
     NSCalendar *calendar = [NSCalendar currentCalendar];
     /**
@@ -65,156 +139,105 @@
     return delta.day;
 }
 
-//获取当前日期星期几
-+ (NSInteger)getWeekNumberWithTime:(NSString *)time
+
+/// 返回n周后的日期
+/// @param timeString 开始计算的日期
+/// @param weekCount 要计算几个星期
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)getTimeAfterWeekCountWithTimeString:(NSString *)timeString
+                                        weekCount:(NSInteger)weekCount
+                                           format:(NSString *)formatString
 {
+    NSDateFormatter *format = [[NSDateFormatter alloc] init];
+    format.dateFormat = formatString;
+    format.timeZone = [NSTimeZone systemTimeZone];
     
-    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
-    formater .timeZone = [NSTimeZone systemTimeZone];
-    [formater setDateFormat:@"yyyyMMdd HHmmss"];//根据自己的需求定义格式
-    // formater.timeZone = [NSTimeZone systemTimeZone];
-    NSDate *inputDate = [formater dateFromString:time];
+    NSDate *beginDate = [format dateFromString:timeString];
     
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     
-    NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+    NSDateComponents *comps = nil;
+    comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
     
-    [calendar setTimeZone: timeZone];
+    NSDateComponents *adcomps = [[NSDateComponents alloc] init];
+    [adcomps setWeekdayOrdinal:weekCount];
     
-    NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;
+    NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
     
-    NSDateComponents *theComponents = [calendar components:calendarUnit fromDate:inputDate];
-    
-    return theComponents.weekday;
+    return [format stringFromDate:start];
 }
 
-//日期计算
-+ (NSString *)getTimeAfter1WeekWithBeginTime:(NSString *)beginTime //返回1周后的日期
-{
-    if ([QTDateCalculationHelper getWeekNumberWithTime:beginTime] == 2) {
-        NSDateFormatter *format = [[NSDateFormatter alloc] init];
-        format.dateFormat = @"yyyyMMdd HHmmss";
-        format.timeZone = [NSTimeZone systemTimeZone];
-        NSDate *beginDate = [format dateFromString:beginTime];
-        NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-        NSDateComponents *comps = nil;
-        comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
-        NSDateComponents *adcomps = [[NSDateComponents alloc] init];
-        
-        [adcomps setWeekdayOrdinal:1];
-        NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
-        
-        return [format stringFromDate:start];
-    }else
-    {
-        NSDateFormatter *format = [[NSDateFormatter alloc] init];
-        format.dateFormat = @"yyyyMMdd HHmmss";
-        format.timeZone = [NSTimeZone systemTimeZone];
-        NSDate *beginDate = [format dateFromString:beginTime];
-        NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-        NSDateComponents *comps = nil;
-        comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
-        NSDateComponents *adcomps = [[NSDateComponents alloc] init];
-        
-        [adcomps setDay:9 - [QTDateCalculationHelper getWeekNumberWithTime:beginTime]];
-        NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
-        
-        return [format stringFromDate:start];
-    }
-    
-    
-}
 
-+ (NSString *)getTimeAfter1MonthWithBeginTime:(NSString *)beginTime
+/// =============== 返回n年,n月,n日后的时间 ===============
+/// @param timeString 开始计算的日期
+/// @param year 年数
+/// @param month 月数
+/// @param day 天数
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)getTimeWithTimeString:(NSString *)timeString
+                               year:(NSInteger)year
+                              month:(NSInteger)month
+                                day:(NSInteger)day
+                             format:(NSString *)formatString
 {
-    NSString *time = [NSString stringWithFormat:@"%@01 000000",[beginTime substringToIndex:6]];
     NSDateFormatter *format = [[NSDateFormatter alloc] init];
-    format.dateFormat = @"yyyyMMdd HHmmss";
+    format.dateFormat = formatString;
     format.timeZone = [NSTimeZone systemTimeZone];
-    NSDate *beginDate = [format dateFromString:time];
+    NSDate *beginDate = [format dateFromString:timeString];
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSDateComponents *comps = nil;
     comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
     NSDateComponents *adcomps = [[NSDateComponents alloc] init];
     
-    [adcomps setMonth:1];
+    [adcomps setYear:year];
+    [adcomps setMonth:month];
+    [adcomps setDay:day];
     NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
     
     return [format stringFromDate:start];
-    
 }
 
-+ (NSString *)getTimeWithBeginTime:(NSString *)beginTime withYears:(NSInteger) Years //返回n年后的日期
+/// =============== 返回n时,n分,n秒后的时间 ===============
+/// @param timeString 开始计算的日期
+/// @param hour 小时数
+/// @param minute 分钟数
+/// @param second 秒数
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)getTimeWithTimeString:(NSString *)timeString
+                               hour:(NSInteger)hour
+                             minute:(NSInteger)minute
+                             second:(NSInteger)second
+                             format:(NSString *)formatString
 {
     NSDateFormatter *format = [[NSDateFormatter alloc] init];
-    format.dateFormat = @"yyyyMMdd HHmmss";
+    format.dateFormat = formatString;
     format.timeZone = [NSTimeZone systemTimeZone];
-    NSDate *beginDate = [format dateFromString:beginTime];
+    NSDate *beginDate = [format dateFromString:timeString];
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSDateComponents *comps = nil;
     comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
     NSDateComponents *adcomps = [[NSDateComponents alloc] init];
     
-    [adcomps setYear:Years];
+    [adcomps setHour:hour];
+    [adcomps setMinute:minute];
+    [adcomps setSecond:second];
     NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
     
     return [format stringFromDate:start];
 }
-+ (NSString *)getTimeWithBeginTime:(NSString *)beginTime withMonths:(NSInteger) months //返回n月后的日期
+
+
+/// =============== 将时间文字转换成多久之前文字 ===============
+/// @param timeString 开始计算的日期
+/// @param formatString 日期字符串的格式,例如:yyyy年MM月dd日
++ (NSString *)formatDateBeforeWithTimeString:(NSString *)timeString
+                                      format:(NSString *)formatString
 {
-    NSDateFormatter *format = [[NSDateFormatter alloc] init];
-    format.dateFormat = @"yyyyMMdd HHmmss";
-    format.timeZone = [NSTimeZone systemTimeZone];
-    NSDate *beginDate = [format dateFromString:beginTime];
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-    NSDateComponents *comps = nil;
-    comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
-    NSDateComponents *adcomps = [[NSDateComponents alloc] init];
-    
-    [adcomps setMonth:months];
-    NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
-    
-    return [format stringFromDate:start];
-}
-+ (NSString *)getTimeWithBeginTime:(NSString *)beginTime withDays:(NSInteger) days    // 返回n天后的日期
-{
-    NSDateFormatter *format = [[NSDateFormatter alloc] init];
-    format.dateFormat = @"yyyyMMdd HHmmss";
-    format.timeZone = [NSTimeZone systemTimeZone];
-    NSDate *beginDate = [format dateFromString:beginTime];
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-    NSDateComponents *comps = nil;
-    comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:beginDate];
-    NSDateComponents *adcomps = [[NSDateComponents alloc] init];
-    
-    [adcomps setDay:days];
-    NSDate *start = [calendar dateByAddingComponents:adcomps toDate:beginDate options:0];
-    return [format stringFromDate:start];
-}
-
-
-
-// 计算与今天相隔几天,正数今天之后,负数今天之前
-+ (NSInteger)daysWithTime:(NSString *)time
-{
-    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
-    [formater setDateFormat:@"yyyyMMdd HHmmss"];//根据自己的需求定义格式
-    NSDate* startDate = [NSDate date];
-    NSDate* endDate = [formater dateFromString:time];
-    NSTimeInterval times = [endDate timeIntervalSinceDate:startDate];
-    
-    
-    return times / 86400;
-    
-}
-
-
-- (NSString *)formatDateBeforeWithStr:(NSString *)str{
     
     NSString *_createDate = @"";
-    NSDate *toDate = [self dateWithString:str format:@"yyyyMMdd HHmmss"];
+    NSDate *toDate = [self getDateWithTimeString:timeString format:formatString];
     NSCalendar *calender =  [NSCalendar currentCalendar];
-    NSDateComponents *dateComponents =  [calender components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute fromDate:toDate toDate:[NSDate date] options:NSCalendarWrapComponents];
+    NSDateComponents *dateComponents =  [calender components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:toDate toDate:[NSDate date] options:NSCalendarWrapComponents];
     if (dateComponents.year > 0) {
         _createDate = [NSString stringWithFormat:@"%ld年前",dateComponents.year];
     }else if (dateComponents.month > 0){
@@ -225,15 +248,12 @@
         _createDate = [NSString stringWithFormat:@"%ld小时前",dateComponents.hour];
     }else if (dateComponents.minute > 0){
         _createDate = [NSString stringWithFormat:@"%ld分钟前",dateComponents.minute];
+    }else if (dateComponents.second > 10){
+        _createDate = [NSString stringWithFormat:@"%ld秒前",dateComponents.second];
     }else{
-        _createDate = [NSString stringWithFormat:@"1分钟前"];
+        _createDate = [NSString stringWithFormat:@"刚刚"];
     }
     return _createDate;
 }
 
-- (NSDate *)dateWithString:(NSString *)dateString format:(NSString *)format {
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:format];
-    return [formatter dateFromString:dateString];
-}
 @end
